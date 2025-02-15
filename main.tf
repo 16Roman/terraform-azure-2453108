bfe4b0312d2e64797d83aa6e36164afd2ea76609
Roman <16romangiannuzzi@gmail.com>
2025-02-15 18:27:58 -0500

    Create VM+RG

--- main.tf
+++ main.tf
@@ -2,14 +2,74 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.90.0"
+      source = "hashicorp/azurerm"
+      version = "4.18.0"
     }
   }
-
-  required_version = ">= 0.14.9"
 }
 
 provider "azurerm" {
-  features {}
+  features{}
+  subscription_id = "ADD ID HERE"
+}
+#Create Resouce group
+resource "azurerm_resource_group" "main" {
+  name = "learn-tf-rg-eastus"
+  location = "eastus"
+}
+#Creates Virtual Network
+resource "azurerm_virtual_network" "main" {
+  name = "learn-tf-vnet-eastus"
+  location = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space = ["10.0.0.0/16"]
+}
+
+#create Subnet
+resource "azurerm_subnet" "main" {
+  name = "learn-tf-subnet-eastus"
+  virtual_network_name = azurerm_virtual_network.main.name
+  resource_group_name = azurerm_resource_group.main.name
+  address_prefixes = ["10.0.0.0/24"]
+}
+
+#Create Network interface card (NIC)
+resource "azurerm_network_interface" "internal" {
+  name = "learn-tf-nic-int-eastus"
+  location = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name = "internal"
+    subnet_id = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+#Create Virtual Machine
+
+resource "azurerm_windows_virtual_machine" "main" {
+  name = "learn-tf-vm-eu"
+  resource_group_name = azurerm_resource_group.main.name
+  location = azurerm_resource_group.main.location
+  size = "Standard_B1s"
+  admin_username = "user_admin"
+  admin_password = "Flashlight8267!"
+
+  network_interface_ids = [
+    azurerm_network_interface.internal.id
+  ]
+
+  os_disk {
+    caching = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer = "WindowsServer"
+    sku = "2016-DataCenter"
+    version = "latest"
+  }
+
 }
